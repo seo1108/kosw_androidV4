12ebb89ead1b984aead33c500f6bb68898044c6e
Obigo <kehdanf-vine@github.com>
2021-11-20 20:22:54 +0900

    foreground fcm receive

--- src/main/java/kr/co/photointerior/kosw/service/fcm/FcmMessagingService.java
+++ src/main/java/kr/co/photointerior/kosw/service/fcm/FcmMessagingService.java
@@ -1,10 +1,29 @@
 package kr.co.photointerior.kosw.service.fcm;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Objects;
+
+import kr.co.photointerior.kosw.R;
 import kr.co.photointerior.kosw.conf.ConditionHolder;
+import kr.co.photointerior.kosw.ui.MainActivity;
 import kr.co.photointerior.kosw.utils.LogUtils;
+
+import static kr.co.photointerior.kosw.conf.AppConst.NOTIFICATION_CHANNEL_ID;
 
 /**
  * 푸시메세지 수신 클래스.
@@ -15,8 +34,38 @@ public class FcmMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        ConditionHolder.instance()
-                .getFcmMessageWorker().work(getApplicationContext(), remoteMessage);
+       /* ConditionHolder.instance()
+                .getFcmMessageWorker().work(getApplicationContext(), remoteMessage);*/
+
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+                //scheduleJob();
+                String title = remoteMessage.getNotification().getTitle();
+                String message = remoteMessage.getNotification().getBody();
+
+                sendNotification(title, message);
+            } else {
+                // Handle message within 10 seconds
+                handleNow();
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+
     }
 
 //    @Override
@@ -25,4 +74,55 @@ public class FcmMessagingService extends FirebaseMessagingService {
 //        ConditionHolder.instance()
 //                .getFcmMessageWorker().work(getApplicationContext(), bundle);
 //    }
+
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        sendRegistrationToServer(token);
+    }
+
+    private void handleNow() {
+        Log.d(TAG, "Short lived task is done.");
+    }
+
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+
+    private void sendNotification(String title, String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = NOTIFICATION_CHANNEL_ID;
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(title)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
 }
